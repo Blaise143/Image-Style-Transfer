6fa247184e97f889d53c78c70e469200e2985c08
Blaise143 <blaiseappo@gmail.com>
2022-12-11 22:24:28 -0800

    cleaned up code

--- Style Transfer.py
+++ Style Transfer.py
@@ -30,39 +30,3 @@ def solve(first_array: list, sec_array: list) -> list:
                 sec_array_copy.remove(num)
     print(final_list)
     return final_list
-
-
-# solve([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 1, 7, 6, 5, 4, 3, 2, 8])
-starry = plt.imread("Image/new_starry.jpg")
-blaise = plt.imread("new_blaise_2.jpeg")
-sunset = plt.imread("Image/sunset.jpeg")
-# print(f"Starry: {starry.shape} \n"
-#       f"Sunset: {sunset.shape} \n"
-#       f"blaise: {blaise.shape}")
-# b = copy.deepcopy(blaise)
-# s = copy.deepcopy(sunset)
-# st = copy.deepcopy(starry)
-# b = b.flatten()
-# s = s.flatten()
-# st = st.flatten()
-# print(b)
-# print(len(b), len(s), len(st))
-
-
-# plt.imshow(blaise)
-# plt.show()
-# new_blaise = copy.deepcopy(blaise)
-# new_blaise = new_blaise.flatten()
-# print(new_blaise)
-# b = new_blaise.reshape(blaise.shape)
-# b = np.rot90(b, k=3)
-# print(b)
-# plt.imshow(b)
-# plt.show()
-
-# print(type(blaise))
-# b_1 = blaise.tolist()
-content = blaise.flatten().tolist()
-style = starry.flatten().tolist()
-# print(type(b))
-solve(content, style)
